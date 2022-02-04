9d019493b3e1a60b92bf066aee0c50cd172588a6
Tom Varewyck <tvarewyck@underside.be>
2022-02-04 13:59:07 +0100

    Auth service + login

--- src/shared/models/user.ts
+++ src/shared/models/user.ts
@@ -1,11 +1,11 @@
 export class User {
-    firstName: string;
-    lastName: string;
-    email: string;
-    
-    constructor(json: any) {
-        this.firstName = json.first_name;
-        this.lastName = json.last_name;
-        this.email = json.email;
-    }
+  uuid: string;
+  email: string;
+  createdAt: string;
+
+  constructor(json: any) {
+    this.uuid = json.uuid; // TODO:: change id to uuid in api respoonse
+    this.email = json.email;
+    this.createdAt = json.createdAt;
+  }
 }
--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { throwError, Observable } from 'rxjs';
+import Config from 'src/app/config/serverUrls.json';
+import { Store } from 'src/store';
+import { User } from 'src/shared/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseRoute: string;
+
+  constructor(private httpClient: HttpClient, private store: Store) {
+    this.baseRoute = environment.serverUrl + Config.prefix + Config.auth;
+  }
+
+  login (parameters: any): Observable<void> {
+    console.log(this.baseRoute);
+    return this.httpClient.post(`${this.baseRoute}/login`, parameters)
+      .pipe(
+        map((result: any) => {
+          console.log(result);
+          localStorage.setItem('token', result.token);
+          const newUser = new User(result.user);
+          this.store.set('connectedUser', newUser);
+        }),
+        catchError(error => throwError(error))
+      );
+  }
+
+  register (parameters: any): Observable<void> {
+    return this.httpClient.post(`${this.baseRoute}/register`, parameters)
+      .pipe(
+        map((result: any) => {
+          localStorage.setItem('token', result.token);
+          const newUser = new User(result.user);
+          this.store.set('connectedUser', newUser);
+        }),
+        catchError(error => throwError(error))
+      )
+  }
 }
