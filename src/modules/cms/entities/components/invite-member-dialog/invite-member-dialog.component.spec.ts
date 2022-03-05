ac08bd23e7d5e7b4d249b9080b566c0609af15cb
VareTom <tomvarewyck98@gmail.com>
2022-03-05 15:43:04 +0100

    Invite member in entity form

---
+++ src/modules/cms/entities/components/invite-member-dialog/invite-member-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InviteMemberDialogComponent } from './invite-member-dialog.component';
+
+describe('InviteMemberDialogComponent', () => {
+  let component: InviteMemberDialogComponent;
+  let fixture: ComponentFixture<InviteMemberDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InviteMemberDialogComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InviteMemberDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/modules/cms/entities/components/invite-member-dialog/invite-member-dialog.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NbDialogRef, NbToastrService } from '@nebular/theme';
+import { TranslateService } from '@ngx-translate/core';
+import { Store } from 'src/store';
+
+// Services
+import { EntityService } from 'src/shared/services/entity.service';
+
+@Component({
+  selector: 'app-invite-member-dialog',
+  templateUrl: './invite-member-dialog.component.html',
+  styleUrls: ['./invite-member-dialog.component.scss']
+})
+export class InviteMemberDialogComponent implements OnInit {
+  
+  memberForm: FormGroup = this.formBuilder.group({
+    email: [null, [Validators.required, Validators.email]]
+  })
+  isSubmitted: boolean = false;
+  submitButtonText: string = this.translate.instant('actions.invite');
+  
+  constructor(protected dialogRef: NbDialogRef<InviteMemberDialogComponent>,
+              private translate: TranslateService,
+              private toastrService: NbToastrService,
+              private readonly entityService: EntityService,
+              private store: Store,
+              private formBuilder: FormBuilder) { }
+  
+  ngOnInit(): void { }
+  
+  get isEmailRequiredInput(): boolean {
+    const formControl = this.memberForm.get('email');
+    return formControl.touched && formControl.getError('required');
+  }
+  
+  get isEmailInvalidInput(): boolean {
+    const formControl = this.memberForm.get('email');
+    return formControl.touched && formControl.getError('email');
+  }
+  
+  onClose(): void {
+    this.dialogRef.close();
+  }
+  
+  onSubmit(): void {
+    this.isSubmitted = true;
+    
+    this.entityService.invite(this.memberForm.value)
+      .subscribe({
+        next: (memberCreated) => {
+          this.isSubmitted = false;
+          this.toastrService.success(this.translate.instant('entity.invitation-succeed'));
+          this.dialogRef.close(memberCreated);
+        },
+        error: (error) => {
+          this.isSubmitted = false;
+          this.toastrService.danger(this.translate.instant('entity.invitation-failed'), this.translate.instant('errors.title'));
+        }
+      })
+  }
+
+}
