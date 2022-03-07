9ff650a9699c58e35b6d228793b6663e39d6c588
VareTom <tomvarewyck98@gmail.com>
2022-03-07 22:11:35 +0100

    Display client details

--- src/shared/models/car.ts
+++ src/shared/models/car.ts
@@ -2,6 +2,7 @@ export class Car {
   uuid: string;
   merch: string;
   model: string;
+  plate: string;
   year?: number;
   color?: string;
   createdAt: string;
@@ -10,6 +11,7 @@ export class Car {
     this.uuid = json.uuid;
     this.merch = json.merch;
     this.model = json.model;
+    this.plate = json.plate;
     this.year = json.year;
     this.color = json.color;
     this.createdAt = json.createdAt;
