a9742d0e6ebdc516f444cb2677eda12d88cf7646
VareTom <tomvarewyck98@gmail.com>
2022-03-08 21:09:12 +0100

    client info dialog component

---
+++ src/modules/cms/clients/components/client-info-dialog/client-info-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-info-dialog',
+  templateUrl: './client-info-dialog.component.html',
+  styleUrls: ['./client-info-dialog.component.scss']
+})
+export class ClientInfoDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
