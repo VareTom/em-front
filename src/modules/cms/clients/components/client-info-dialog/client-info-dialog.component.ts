ca6978ff9907f3d5a7d19a0338345e852cac82b7
VareTom <tomvarewyck98@gmail.com>
2022-03-08 21:44:15 +0100

    Update client info

--- src/modules/cms/clients/components/client-info-dialog/client-info-dialog.component.ts
+++ src/modules/cms/clients/components/client-info-dialog/client-info-dialog.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NbDialogRef } from '@nebular/theme';
+
+// Models
+import { Client } from 'src/shared/models/client';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-client-info-dialog',
@@ -6,10 +12,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-info-dialog.component.scss']
 })
 export class ClientInfoDialogComponent implements OnInit {
-
-  constructor() { }
+  
+  clientToUpdate: Client;
+  
+  clientInfoForm: FormGroup = this.formBuilder.group({
+    firstName: [null, Validators.required],
+    lastName: [null]
+  })
+  submitButtonText: string = this.translate.instant('actions.create');
+  
+  constructor(protected dialogRef: NbDialogRef<ClientInfoDialogComponent>,
+              private translate: TranslateService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    if (this.clientToUpdate) {
+      this.submitButtonText = this.translate.instant('actions.update');
+      this.clientInfoForm.patchValue({
+        firstName: this.clientToUpdate.firstName,
+        lastName: this.clientToUpdate.lastName
+      })
+    }
   }
-
+  
+  get isFirstNameRequiredInput(): boolean {
+    const formControl = this.clientInfoForm.get('firstName');
+    return formControl.touched && formControl.getError('required');
+  }
+  
+  onSubmit(): void {
+    this.dialogRef.close(this.clientInfoForm.value);
+  }
+  
+  onClose(): void {
+    this.dialogRef.close(false);
+  }
 }
